b957111246cca335d2aacb7eb6b6506200962aff
Sanket Parsewar <sanket.parsewar97@gmail.com>
2024-10-09 23:15:38 +0530

    update

--- src/app/components/apiIntegration/get-api/get-api.component.ts
+++ src/app/components/apiIntegration/get-api/get-api.component.ts
@@ -1,26 +1,32 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-get-api',
+  selector: "app-get-api",
   standalone: true,
   imports: [],
-  templateUrl: './get-api.component.html',
-  styleUrl: './get-api.component.css',
+  templateUrl: "./get-api.component.html",
+  styleUrl: "./get-api.component.css",
 })
 export class GetApiComponent {
-
-  userList:any;
+  userList: any;
+  customerList: any;
   // dependency injection
   // create object of HttpClient
-  constructor(private http:HttpClient){
-
-  }
+  constructor(private http: HttpClient) {}
   // this is common code to call the api
-  getAllUser(){
-    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((result:any)=>{
-      this.userList=result;
-    })
+  getAllUser() {
+    this.http
+      .get("https://jsonplaceholder.typicode.com/users")
+      .subscribe((result: any) => {
+        this.userList = result;
+      });
   }
-
+  getAllCustomer() {
+    this.http
+      .get("https://projectapi.gerasim.in/api/RealEstate/GetAllCustomers")
+      .subscribe((res: any) => {
+        this.customerList = res.data;
+      });
+  }
 }
